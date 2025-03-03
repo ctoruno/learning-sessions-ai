b77381959930eddf6320707a5249c0e1ff46985a
Carlos A. Toruño P. <carlos.toruno@gmail.com>
2025-03-03 10:23:48 -0500

    Update ai.py

--- tools/ai.py
+++ tools/ai.py
@@ -86,9 +86,10 @@ def gen_context_prompt(transcript):
     Asegúrate de que tu retroalimentación sea:
 
     - Objetiva, alentadora y enfocada en el crecimiento profesional del tutor.
-    - Limitada a 800 caracteres.
+    - Limitada a 1000 caracteres.
     - Escrita en formato de lista con viñetas, sin títulos ni encabezados.
-    - Directa y clara, ya que será leída directamente por el tutor.
+    - Directa, clara y utilizando un tono amable, ya que será leída directamente por el tutor.
+    - Contextualizada, es decir, procura añadir 1 o 2 ejemplos de cómo el tutor podría haber implementado tus recomendaciones en la sesión previa
 
     ASPECTOS A EVALUAR:
 
@@ -103,8 +104,8 @@ def gen_context_prompt(transcript):
     - Concéntrate en la forma en que el tutor enseña, no en modificar el contenido de la guía de ejercicios.
     - Evita resaltar aspectos positivos, salvo para contrastarlos con áreas de mejora.
     - Recuerda que el estudiante tiene entre 8 y 10 años, por lo que las explicaciones deben ser adecuadas a su nivel.
+    - Las sesiones de tutoría se realizan vía telefónica, por lo que el tutor no puede hacer uso de medios visuales, solo auditivos.
     - Enfoca tu set de recomendaciones EXCLUSIVAMENTE en los aspectos a mejorar y en las acciones específicas para lograrlo.
-    - Procura ser amable en tus recomendaciones.
 
     Gracias por tu valiosa contribución a la mejora de la calidad educativa.
     '''
